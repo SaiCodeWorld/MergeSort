845b52f98a0abe244ab929ae927e3c01ee5f885a
SaiCodeWorld <sairam.siva2022@gmail.com>
2021-08-03 13:20:16 -0500

    Began my test code in main.cpp and included all necessary libraries and files in main.h to test the MergeSort class.

--- main.cpp
+++ main.cpp
@@ -3,3 +3,12 @@ Name: Sai Sivakumar
 Merge Sort With Arrays C++
 Purpose: 1-3 sentences about your program.
 **********************/
+
+#include "main.h"
+
+int main (int argc, char **argv)
+{
+    MergeSort mergeTest;
+    int testThis[] = {9, 8, 7, 6, 5, 4, 3, 2};
+    mergeTest.sorter(testThis);
+}
--- main.h
+++ main.h
@@ -3,3 +3,14 @@ Name: Sai Sivakumar
 Merge Sort With Arrays C++
 Purpose: 1-3 sentences about your program.
 **********************/
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+#include "mergeSort.h"
+
+using std::cout;
+using std::endl;
+
+#endif
