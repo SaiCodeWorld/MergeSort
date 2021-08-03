cf88d42b6ac7ba05d61118c51ccb62fbf3f60f1c
SaiCodeWorld <sairam.siva2022@gmail.com>
2021-08-03 18:07:07 -0500

    Removed include for <iostream> and using std::cout and endl statements as I will not be printing in main.cpp

--- main.h
+++ main.h
@@ -7,11 +7,7 @@ Purpose: 1-3 sentences about your program.
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <iostream>
 #include "mergeSort.h"
 #include <cstdlib>
-
-using std::cout;
-using std::endl;
 
 #endif
