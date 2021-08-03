4558eb1386ce7020c0c1d8f6e40d0b35cca92f3d
SaiCodeWorld <sairam.siva2022@gmail.com>
2021-08-03 12:16:19 -0500

    Began my prototyping of the MergeSort Class in mergeSort.h

--- mergeSort.h
+++ mergeSort.h
@@ -3,3 +3,23 @@ Name: Sai Sivakumar
 Merge Sort With Arrays C++
 Purpose: 1-3 sentences about your program.
 **********************/
+#ifndef MERGE_SORT_H
+#define MERGE_SORT_H
+
+#define MAX_SIZE 8
+
+class MergeSort
+{
+    public:
+        MergeSort();
+        ~MergeSort();
+
+        bool sorter(int []);
+
+    private:
+        int origArr[MAX_SIZE];
+        
+        void mergeSort(int [], int first, int last);
+        void merge(int [], int first, int mid, int last);
+};
+#endif //MERGE_SORT_H
